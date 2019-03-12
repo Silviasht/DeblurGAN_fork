44f061ede4b1d7514c163883fe33a2c548b8b7b2
Silviasht <46275058+Silviasht@users.noreply.github.com>
2019-03-12 16:05:02 +0800

    Update test_model.py

--- models/test_model.py
+++ models/test_model.py
@@ -14,6 +14,9 @@ class TestModel(BaseModel):
         BaseModel.initialize(self, opt)
         self.input_A = self.Tensor(opt.batchSize, opt.input_nc, opt.fineSize, opt.fineSize)
 
+        self.input_B = self.Tensor(opt.batchSize, opt.output_nc,
+                                   opt.fineSize, opt.fineSize)
+
         self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf,
                                       opt.which_model_netG, opt.norm, not opt.no_dropout, self.gpu_ids, False,
                                       opt.learn_residual)
@@ -27,14 +30,19 @@ class TestModel(BaseModel):
     def set_input(self, input):
         # we need to use single_dataset mode
         input_A = input['A']
+        input_B = input['B']
         temp = self.input_A.clone()
         temp.resize_(input_A.size()).copy_(input_A)
+
+        self.input_B.resize_(input_B.size()).copy_(input_B)
+
         self.input_A = temp
         self.image_paths = input['A_paths']
 
     def test(self):
-        self.real_A = Variable(self.input_A, volatile=True)
+        self.real_A = Variable(self.input_A)
         self.fake_B = self.netG.forward(self.real_A)
+        self.real_B = Variable(self.input_B)   #########
 
     # get image paths
     def get_image_paths(self):
@@ -43,4 +51,6 @@ class TestModel(BaseModel):
     def get_current_visuals(self):
         real_A = util.tensor2im(self.real_A.data)
         fake_B = util.tensor2im(self.fake_B.data)
-        return OrderedDict([('real_A', real_A), ('fake_B', fake_B)])
+
+        real_B = util.tensor2im(self.real_B.data)
+        return OrderedDict([('real_A', real_A), ('fake_B', fake_B),('real_B',real_B)])
